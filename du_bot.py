4526514043eda796e5912d84c418465507360f45
JaeHyeonSung <top91652@gmail.com>
2023-05-28 13:22:47 +0900

    In/Out function added

--- du_bot.py
+++ du_bot.py
@@ -14,5 +14,15 @@ async def on_ready():
 @bot.command()
 async def hello(message):
     await message.channel.send('Hi!')
+@bot.command()
+async def 들어와(message):
+    channel = message.author.voice.channel
+    await channel.connect()
+    await message.channel.send("대구대 봇 입장")
+
+@bot.command()
+async def 나가(message):
+    await bot.voice_clients[0].disconnect()
+    await message.channel.send("대구대 봇 퇴장")
 
 bot.run('Token')
